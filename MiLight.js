c872f9f062dc41e832b76f730e53a4eaa59e7cd3
Daniel Thomas <daniel@networklighthouse.com>
2015-12-31 03:40:54 +0000

    Milight objects (RGB, White, Power) and package.json tidy up.

--- MiLight.js
+++ MiLight.js
@@ -1,18 +1,60 @@
 module.exports = function(RED) {
+    function MiLightPower(config) {
+	RED.nodes.createNode(this,config);
+	var node = this;
+   	var MiLight = require('milight');
+	var milight = new MiLight({
+		host: config.ip,
+		broadcast: config.broadcast
+	});
+   	var zone = config.zone;
+	this.on('input', function(msg) {
+		if (msg.payload == "off") milight.zone(zone).off();
+		if (msg.payload == "on") milight.zone(zone).on();
+	});
+    };
+ RED.nodes.registerType("MiLightPower",MiLightPower);
+
     function MiLightRGB(config) {
 	RED.nodes.createNode(this,config);
 	var node = this;
    	var MiLight = require('milight');
 
 	var milight = new MiLight({
-		host: "192.168.0.255",
-		broadcast: true
-	});
-   	var zone = 1;
+                host: config.ip,
+                broadcast: config.broadcast
+        });
+        var zone = config.zone;
+
+
 	this.on('input', function(msg) {
-		milight.zone(zone).rgb(msg.payload);
-		});
+		if (typeof msg.payload == "string") {	
+			milight.zone(zone).rgb(msg.payload);
+		} else {
+			milight.zone(zone).rgb(msg.payload.rgb || "#FFFFFF");
+			milight.zone(zone).brightness(msg.payload.brightness) || 100;
+		}
+	});
     };
  RED.nodes.registerType("MiLightRGB",MiLightRGB);
+
+    function MiLightWhite(config) {
+	RED.nodes.createNode(this,config);
+	var node = this;
+   	var MiLight = require('milight');
+	
+	var milight = new MiLight({
+                host: config.ip,
+                broadcast: config.broadcast
+        });
+        var zone = config.zone;
+	
+
+	this.on('input', function(msg) {
+		milight.zone(zone).white(msg.payload);
+	});
+    };
+ RED.nodes.registerType("MiLightWhite",MiLightWhite);
+
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # node-red-contrib-milight
 A Node Red wrapper for https://github.com/oeuillot/node-milight.git
+Currently presents 3 Nodes
+
+* Milight RGB (set RGB colors)
+* Milight White (set white mode brightness)
+* Milight Power (on/off control)
+## To Do
+Create a single Node which encompasses all functionality
+
+## Known Issues
+
+* MiLightRGB has brightness controls, however causes an odd flicker when changing
+
+
+
